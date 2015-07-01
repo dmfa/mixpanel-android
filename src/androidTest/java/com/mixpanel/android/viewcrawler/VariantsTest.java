cf173067583e31cb356d1fd78e98fd7fc594e5cf
Joe <joe@mixpanel.com>
2015-07-01 10:43:11 -0700

    WIP untested unification of tweaks pipeline with changes pipeline, in advance of test+refactor

---
+++ src/androidTest/java/com/mixpanel/android/viewcrawler/VariantsTest.java
@@ -0,0 +1,34 @@
+package com.mixpanel.android.viewcrawler;
+
+
+import android.test.AndroidTestCase;
+
+public class VariantsTest extends AndroidTestCase {
+    public void setUp() {
+
+    }
+
+    public void testTweaksOnlyPersistentVariant() {
+        fail("Write this test");
+    }
+
+    public void testTweaksOnlyEdit() {
+        fail("Write this test");
+    }
+
+    public void testChangesOnlyPersistentVariant() {
+        fail("Write this test");
+    }
+
+    public void testChangesOnlyEditt() {
+        fail("Write this test");
+    }
+
+    public void testTweaksAndChangesPersistentVariant() {
+        fail("Write this test");
+    }
+
+    public void testTweaksAndChangesEdit() {
+        fail("Write this test");
+    }
+}
